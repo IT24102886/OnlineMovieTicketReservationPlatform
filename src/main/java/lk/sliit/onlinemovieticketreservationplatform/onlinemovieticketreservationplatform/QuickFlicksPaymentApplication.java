c5da8f8437959e154a270f2518cdf7cc72bc9757
it24102895 <it24102895@my.sliit.lk>
2025-05-22 09:50:29 +0530

    Add QuickFlicksPaymentApplication

---
+++ src/main/java/lk/sliit/onlinemovieticketreservationplatform/onlinemovieticketreservationplatform/QuickFlicksPaymentApplication.java
@@ -0,0 +1,12 @@
+package com.quickflicks.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickFlicksPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickFlicksPaymentApplication.class, args);
+    }
+}
